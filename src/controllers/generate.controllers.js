3dc353e85243053a882a343232845889493b3337
sahil78tt <sahilvishwakarma2005@gmail.com>
2026-09-03 00:42:08 +0530

    Get  api responses

--- src/controllers/generate.controllers.js
+++ src/controllers/generate.controllers.js
@@ -1,4 +1,7 @@
 import prompt from "../models/prompts.js";
+import history from "../models/history.js";
+
+const GROQ_URL = "https://api.groq.com/openai/v1/chat/completions";
 
 export const generate = async (req, res) => {
   try {
@@ -8,17 +11,44 @@ export const generate = async (req, res) => {
       return res.status(400).json({ message: "User input not found" });
     }
 
-    const currentPrompt = await prompt.findById("Education Prompt");
+    const existingPrompt = await prompt.findById("Education Prompt");
 
-    if (!currentPrompt) {
+    if (!existingPrompt) {
       return res.status(400).json({ message: "Prompt not found" });
     }
 
-    const newPrompt = currentPrompt.template.replace("{userInput}", userInput);
+    const updatedPrompt = existingPrompt.template.replace(
+      "{userInput}",
+      userInput,
+    );
+
+    const groqResponse = await fetch(GROQ_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.GROQ_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "openai/gpt-oss-20b",
+        messages: [{ role: "user", content: updatedPrompt }],
+      }),
+    });
+
+    if (!groqResponse.ok) {
+      return res.status(400).json({ message: "Error fetching API" });
+    }
+
+    const data = await groqResponse.json();
+    const response = data.choices[0]?.message?.content;
+
+    const saveHistory = await new history({
+      userInput: userInput,
+      response: response,
+    }).save();
 
     res
       .status(200)
-      .json({ message: "Prompt generated successfully", prompt: newPrompt });
+      .json({ message: "Prompt generated successfully", prompt: response });
   } catch (error) {
     console.log(`error in generate controller : ${error}`);
     res.status(500).json({ message: "Internal server error" });
